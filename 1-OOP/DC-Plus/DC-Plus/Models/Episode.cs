321e9b85c559c6161f426f4955d131adda0e330a
farkas.norbert <farkas.norbert@ckik.hu>
2026-09-10 10:29:25 +0200

    Episode Parse

--- 1-OOP/DC-Plus/DC-Plus/Models/Episode.cs
+++ 1-OOP/DC-Plus/DC-Plus/Models/Episode.cs
@@ -2,7 +2,7 @@
 {
     internal class Episode
     {
-        public Episode(int id, int seriesId, string title, DateTime releaseDate, int season, int episodeNumber, int duration)
+        public Episode(int id, int seriesId, string title, DateTime releaseDate, int season, int episodeNumber, int duration, int viewCount = 0)
         {
             Id = id;
             SeriesId = seriesId;
@@ -11,6 +11,7 @@
             Season = season;
             EpisodeNumber = episodeNumber;
             Duration = duration;
+            ViewCount = viewCount;
         }
 
         public int Id { get; }
@@ -20,11 +21,26 @@
         public int Season { get; } // 2. évad
         public int EpisodeNumber { get; } // 3. epizód
         public int Duration { get; }
+        public int ViewCount { get; }
 
         // "A Skorpió csípése - S1E6"
         public override string ToString()
         {
             return $"{Title} - S{Season:00}E{EpisodeNumber:00}";
+        }
+
+        public static Episode Parse(string line)
+        {
+            string[] data = line.Split(";");
+            Episode episode = new(id: int.Parse(data[0]),
+                                  seriesId: int.Parse(data[1]),
+                                  title: data[2],
+                                  season: int.Parse(data[3]),
+                                  episodeNumber: int.Parse(data[4]),
+                                  duration: int.Parse(data[5]),
+                                  releaseDate: DateTime.Parse(data[6]),
+                                  viewCount: int.Parse(data[7]));
+            return episode;
         }
     }
 }
